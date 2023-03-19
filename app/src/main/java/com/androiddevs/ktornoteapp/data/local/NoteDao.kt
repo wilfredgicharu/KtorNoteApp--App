d7a3ee2b3130ff3b98fa757f7363a4acaa8e0159
wilfred gicharu <wilfredgcharu254@gmail.com>
2023-03-19 23:13:09 +0300

    adding database

--- app/src/main/java/com/androiddevs/ktornoteapp/data/local/NoteDao.kt
+++ app/src/main/java/com/androiddevs/ktornoteapp/data/local/NoteDao.kt
@@ -1,10 +1,12 @@
 package com.androiddevs.ktornoteapp.data.local
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.androiddevs.ktornoteapp.data.local.entities.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDao {
@@ -16,5 +18,19 @@ interface NoteDao {
 
     @Query("DELETE FROM notes WHERE isSynced = 1")
     suspend fun deleteAllSyncedNotes()
+
+    @Query("SELECT * FROM notes WHERE id = :noteID")
+    fun observeNoteById(noteID: String) : LiveData<Note>
+
+    @Query("SELECT * FROM notes WHERE id = :noteID")
+    suspend fun getNoteById(noteID: String) : Note?
+
+    @Query("SELECT * FROM notes ORDER BY date DESC")
+    fun getAllNotes(): Flow<List<Note>>
+
+    @Query("SELECT * FROM notes WHERE isSynced = 0")
+    suspend fun getAllUnsyncedNotes(): List<Note>
+
+
 
 }
