55d616412e443b2fc9d9d4242efbe0ad75292920
wilfred gicharu <wilfredgcharu254@gmail.com>
2023-03-25 23:46:47 +0300

    bug fix

---
+++ app/src/main/java/com/androiddevs/ktornoteapp/other/Constants.kt
@@ -0,0 +1,16 @@
+package com.androiddevs.ktornoteapp.other
+
+object Constants {
+    const val DATABASE_NAME = "notes_db"
+
+    const val KEY_LOGGED_IN_EMAIL ="KEY_LOGGED_IN_EMAIL"
+    const val KEY_PASSWORD ="KEY_PASSWORD"
+    const val NO_EMAIL ="NO_EMAIL"
+    const val NO_PASSWORD ="NO_PASSWORD"
+
+    const val ENCRYPTED_SHARED_PREF_NAME = "enc_shared_pref"
+
+    const val BASE_URL = "http://10.0.2.2:8001"
+
+    val IGNORE_AUTH_URLS = listOf("/login", "/register")
+}
